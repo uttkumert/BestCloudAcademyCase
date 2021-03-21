4e8f04c119ab44c9a64aab0e32ef623e9a91450c
Utku Mert <utkumert@Utku-MacBook-Pro.local>
2021-03-21 15:51:01 +0300

    a

--- BCFM_Case/app.py
+++ BCFM_Case/app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 names = {"firstname": "utku", "lastname": "mert"}
 
 @app.route("/whoami")
-def x1():
+def whoami_():
     return jsonify(names)
 
 @app.route("/alert")
-def x2():
+def alert_():
     url = 'https://webhook.site/26ea7387-4afc-4c0b-ace9-3e429f161b8a'
     r = requests.post('https://webhook.site/26ea7387-4afc-4c0b-ace9-3e429f161b8a', data = names)
-    return r.text
+    return jsonify(names)
     
     
